bc47e9f0f8f08d44ccba4e81f1e8e30550478d39
sept1st2c <sept1st2c@gmail.com>
2024-07-06 09:07:18 +0530

    organising routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,80 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-
-const prisma = new PrismaClient({
-  datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate());
+import { decode, sign, verify } from "hono/jwt";
 
 // Create the main Hono app
-const app = new Hono();
-app.get("/", (c) => {
-  return c.text("hi");
-});
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+//app.get("/", async (c) => {});
+app.post("/api/v1/signup", async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.text("jwt here" + jwt);
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Invalid");
+  }
+  // return c.text("signup route");
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
+app.post("/api/v1/signin", async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(411);
+      return c.json({ error: "user not found" });
+    }
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.text("jwt here" + jwt);
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Invalid");
+  }
 });
 
 app.get("/api/v1/blog/:id", (c) => {
